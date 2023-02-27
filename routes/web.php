fc37324812a242b1c417cf0e2bc1b9c981189bc8
erikcmoo <erik.carrillo@tecmilenio.mx>
2023-02-26 21:50:23 -0600

    Guardamos cambios version demo

---
+++ routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmpleadoController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/trabajador', function () {
+    return view('Trabajador.create');
+});
+
+Route::get('/trabajador/create', function () {
+    return view('Trabajador.create');
+});
+
+Route::get('/trabajador/edit', function () {
+    return view('Trabajador.edit');
+});
+
+
+
+
+
+/*
+
+Route::get('/empleado', function () {
+    return view('Empleado.index');
+});
+
+Route::get('/empleado/create',[EmpleadoController::class,'create']);
+*/
+
